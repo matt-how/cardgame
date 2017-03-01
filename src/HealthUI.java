c2a9f6f40195abab4dd5f935c1d240062988a942
GiglioCapurro <gigliocapurro@gmail.com>
2017-03-01 20:14:14 +0000

    Added comments
    Added comments and made minor code changes that shouldn't actually change anything.

--- src/HealthUI.java
+++ src/HealthUI.java
@@ -14,21 +14,19 @@ public class HealthUI {
 	private CircleShape elementType;
 	private CircleShape elementBorder;
 
-	public HealthUI(int hp, int maxHP, Character.elementalType ourElement, int x, int y) {
+	public HealthUI(int hp, int maxHP, Character.elementalType ourElement, int x, int y) {//relevant carried over info
 
-		if(hp !=0) {
-			healthDif = 60 / maxHP;
-		}
+		healthDif = 60 / maxHP; //finds value for scaling
 
-		healthBar = new RectangleShape(new Vector2f((hp*healthDif), 10));
+		healthBar = new RectangleShape(new Vector2f((hp*healthDif), 10));//makes healthbar
 		healthBar.setFillColor(Color.GREEN);
 		healthBar.setPosition((x + 5), (y - 10));
 
-		healthBorder = new RectangleShape(new Vector2f(60, 10));
+		healthBorder = new RectangleShape(new Vector2f(60, 10));//makes background for healthbar
 		healthBorder.setFillColor(Color.BLACK);
 		healthBorder.setPosition((x + 5), (y - 10));
 
-		if(ourElement == Character.elementalType.NONE) {
+		if(ourElement == Character.elementalType.NONE) {//checks for element if any
 			elementNone = true;
 		}else{
 			elementType = new CircleShape(10);
@@ -37,7 +35,7 @@ public class HealthUI {
 			Texture imgTexture = new Texture();
 
 			try {
-				imgTexture.loadFromFile(Paths.get("eType" + ourElement + ".png"));
+				imgTexture.loadFromFile(Paths.get("eType" + ourElement + ".png"));//sets icon for element type
 			}catch (IOException ex) {
 				ex.printStackTrace();
 			}
@@ -45,10 +43,9 @@ public class HealthUI {
 		}
 	}
 
-	public void draw(RenderWindow window) {
+	public void draw(RenderWindow window) {//draws healthbar and icons
 		window.draw(healthBorder,RenderStates.DEFAULT);
 		window.draw(healthBar,RenderStates.DEFAULT);
-//		window.draw(elementBorder, RenderStates.DEFAULT);
 		if(elementNone == false) {
 			window.draw(elementType, RenderStates.DEFAULT);
 		}
